ea405519c2f9a04f0cb24626c293aeb799d72007
nakada <ddt.nakadas@gmail.com>
2021-06-02 20:02:01 +0900

    演習1-3のコミット

--- E_01/src/E_01_01/E_01_01.java
+++ E_01/src/E_01_01/E_01_01.java
@@ -7,7 +7,7 @@ package E_01_01;
 public class E_01_01 {
 
 	public static void main(String[] args) {
-		System.out.println("初めてのjavaプログラム")
+		System.out.println("初めてのjavaプログラム");
 		System.out.println("画面に出力しています");
 	}
 
