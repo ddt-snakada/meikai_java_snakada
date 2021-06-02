319eeea7bc835eb222a5bd98bb24b75d4918dbc3
nakada <ddt.nakadas@gmail.com>
2021-06-02 21:13:07 +0900

    演習2-3のコミット

---
+++ chap2/src/chap2_3/E02_03.java
@@ -0,0 +1,17 @@
+//右に示すように、キーボードから読み込んだ整数値をそのまま反復して表示するプログラムを作成せよ。
+package chap2_3;
+// javaのスキャナークラスをインポートする
+import java.util.Scanner;
+
+public class E02_03 {
+	// main関数を定義する
+	public static void main(String[] args) {
+		// スキャナークラスのインスタンスを作成
+		Scanner stdIn = new Scanner(System.in);
+		// 整数値を読み込む
+		int Number = stdIn.nextInt();
+		// 画面に出力する
+		System.out.println(Number + "と入力しましたね。");
+	}
+
+}
