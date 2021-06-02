00ce5770882f21ca81403c4ff8d00045ac01931b
nakada <ddt.nakadas@gmail.com>
2021-06-02 20:03:32 +0900

    演習1-3のコミット

---
+++ chap1/src/chap1_3/E01_03.java
@@ -0,0 +1,9 @@
+/* 1-3右に示すように、各行に１文字ずつ名前を表示するプログラムを作成せよ。姓と名の間は1行あけることとし著者の名前ではなく、自分の名前を表示すること*/
+package chap1_3;
+
+public class E01_03 {
+	public static void main(String[] args) {
+		//git上で公開されるので本名を避け著者を表示する \nは改行を表す
+		System.out.println("柴\n田\n　\n望\n洋");
+	}
+}
