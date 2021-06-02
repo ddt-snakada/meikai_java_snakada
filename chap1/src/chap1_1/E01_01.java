cf848f4819d594bdbe340b2f4cb2b6cf5e8ac638
nakada <ddt.nakadas@gmail.com>
2021-06-02 19:54:33 +0900

    演習1-1のコミット

---
+++ chap1/src/chap1_1/E01_01.java
@@ -0,0 +1,13 @@
+/*プログラム中の終端を示す;が欠如しているとどうなるか。プログラムをコンパイルして検証せよ。 */
+
+/* ; がないことによりコンパイル時にエラーがでてコンパイルできない
+ * 下記は検証したプログラム*/
+package chap1_1;
+
+public class E01_01 {
+	public static void main(String[] args) {
+		//エラーがでるためコメントアウト
+		//System.out.println("初めてのjavaプログラム");
+		System.out.println("画面に出力しています");
+	}
+}
