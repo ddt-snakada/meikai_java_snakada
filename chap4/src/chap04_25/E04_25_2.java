596cc24306236c58d901be0fa518968bd09d12e3
nakada <ddt.nakadas@gmail.com>
2022-04-02 23:43:42 +0900

    演習4-21_25のコミット

---
+++ chap4/src/chap04_25/E04_25_2.java
@@ -0,0 +1,42 @@
+/*正の整数値を読み込み、それが素数であるかどうかを判定するプログラムを作成せよ。素数とは2以上n未満のいずれの数でも割り切れない整数nのことである*/
+package chap04_25;
+
+import java.util.Scanner;
+
+public class E04_25_2 {
+	public static void main(String[] args) {
+		// スキャナクラスのインスタンスを生成する
+		Scanner stdIn = new Scanner(System.in);
+		//説明の表示
+		System.out.println("整数を加算します。");
+		// 読み込みの表示
+		System.out.print("何個加算しますか:");
+		// 整数値を読み込む
+		int n = stdIn.nextInt();
+		//合計値の定義をする
+		int sum = 0;
+		//平均を定義する
+		int average = 0;
+		//読み込んだ整数値分ループする
+		for (int i = 0; i < n; i++) {
+			//説明と読み込みのの表示
+			System.out.print("整数:");
+			// 整数値を読み込む
+			int t = stdIn.nextInt();
+			if(sum + t > 1000) {
+				//0の場合ループから抜けるテキスト指定なのブロックにはしない
+				System.out.println("合計が1000をこえました。");
+				System.out.println("最後の数値は無視します。");
+				break;
+			}			
+			//合計を加算する
+			sum+=t;
+			//平均を計算する iが0からはじまるので1加算する
+			average = sum / (i + 1);
+		}
+		//合計の出力
+		System.out.println("合計は" + sum + "です。");
+		//平均の出力
+		System.out.println("平均は" + average + "です。");
+	}
+}
