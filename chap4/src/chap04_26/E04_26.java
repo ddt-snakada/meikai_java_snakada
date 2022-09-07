bafef07e7e4d21eea3d1e6d1f6b0ccf2aae83f37
nakada <ddt.nakadas@gmail.com>
2022-09-07 19:52:39 +0900

    指摘点の修正コミット

--- chap4/src/chap04_26/E04_26.java
+++ chap4/src/chap04_26/E04_26.java
@@ -7,34 +7,46 @@ public class E04_26 {
 	public static void main(String[] args) {
 		// スキャナクラスのインスタンスを生成する
 		Scanner stdIn = new Scanner(System.in);
-		//説明の表示
+		// 説明の表示
 		System.out.println("整数を加算します。");
 		// 読み込みの表示
 		System.out.print("何個加算しますか:");
 		// 整数値を読み込む
 		int n = stdIn.nextInt();
-		//合計値の定義をする
+		// 合計値の定義をする
 		int sum = 0;
-		//平均を定義する
+		// 平均を定義する
 		int average = 0;
-		//読み込んだ整数値分ループする
+		// 分母を定義する
+		int denominator = 0;
+		// 読み込んだ整数値分ループする
 		for (int i = 0; i < n; i++) {
-			//説明と読み込みのの表示
+			// 説明と読み込みのの表示
 			System.out.print("整数:");
 			// 整数値を読み込む
 			int t = stdIn.nextInt();
-			if(t < 0) {
-				System.out.println("負の数は加算しません。");
-				continue;
-			}			
-			//合計を加算する
-			sum+=t;
-			//平均を計算する iが0からはじまるので1加算する
-			average = sum / (i + 1);
+			// 合計を加算する
+			sum += t;
+			// 読み込んだ整数値が正の数の場合
+			if (t >= 0) {
+				// 分母に1加算する
+				denominator += 1;
+			}
+			// 平均を計算する 0除算がありえるので0以外の場合
+			if (denominator != 0) {
+				// 平均の計算
+				average = sum / denominator;
+			}
 		}
-		//合計の出力
+		// 合計の出力
 		System.out.println("合計は" + sum + "です。");
-		//平均の出力
-		System.out.println("平均は" + average + "です。");
+		//0除算でない場合
+		if (denominator != 0) {
+			// 平均の出力
+			System.out.println("平均は" + average + "です。");
+		//0除算の場合
+		}else {
+			System.out.println("分母が0です。");
+		}
 	}
 }
