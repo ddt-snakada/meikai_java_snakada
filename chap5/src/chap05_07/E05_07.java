f192a58ad5a49029dfeaa2b8bdc1eb5fd07420cd
nakada <ddt.nakadas@gmail.com>
2022-10-05 20:37:43 +0900

    指摘点の修正 ループをint型のものを使うように修正。また一部ソースに誤りがあったため修正

--- chap5/src/chap05_07/E05_07.java
+++ chap5/src/chap05_07/E05_07.java
@@ -5,12 +5,16 @@ public class E05_07 {
 	public static void main(String[] args) {
 		// 2乗を表示する数の定義
 		double squareNumber;
-		// ループを1000回する floatNumberは0.001を足していき
-		for (float floatNumber = 0.0F; floatNumber <= 1.0F; floatNumber += 0.001F) {
+		// フロートの変数の宣言
+		float floatNumber = 0; 
+		// ループを1000回する floatNumberは0.001を足していく
+		for (int loop = 0; loop < 1000; floatNumber += 0.001F,loop++) {
 			// 表示用変数に２乗を代入する
 			squareNumber = floatNumber * floatNumber;
+			// floatNumberを10桁表示
+			System.out.printf("%10.9f\n", floatNumber);			
 			// squareNumberを10桁表示
-			System.out.printf("%f10\n", squareNumber);
+			System.out.printf("%10.9f\n", squareNumber);
 		}
 	}
 
